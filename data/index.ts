4918beeb390fc6579e826fd8d9fa0864620c22e2
Spike Ice <spikeyice.1@gmail.com>
2025-06-10 14:24:41 +0300

    Initial Commit

---
+++ data/index.ts
@@ -0,0 +1,217 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building a modern UI/UX website.",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "3D Solar System Planets to Explore",
+    des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
+    img: "/p1.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+    link: "/ui.earth.com",
+  },
+  {
+    id: 2,
+    title: "Yoom - Video Conferencing App",
+    des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
+    img: "/p2.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    link: "/ui.yoom.com",
+  },
+  {
+    id: 3,
+    title: "AI Image SaaS - Canva Application",
+    des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
+    img: "/p3.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    link: "/ui.aiimg.com",
+  },
+  {
+    id: 4,
+    title: "Animated Apple Iphone 3D Website",
+    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+    img: "/p4.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "/ui.apple.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Working with Yomna was truly impressive. Her professionalism, diligence, and commitment to mastering cybersecurity concepts were evident throughout her internship. Yomna's enthusiasm for penetration testing and capture-the-flag challenges, combined with her ability to tackle complex security problems, was remarkable. If you're looking for a dedicated and talented individual to strengthen your cybersecurity initiatives, Yomna is an outstanding choice.",
+    name: "Dr. Alaa Eldin",
+    title: "Head of Information Systems Department, Misr University for Science and Technology",
+  },
+  {
+    quote:
+      "Collaborating with Yomna was an absolute pleasure. Her professionalism, promptness, and dedication to delivering robust backend solutions were evident throughout her internship. Yomna's enthusiasm for full stack web development, particularly in crafting scalable APIs and optimizing server-side logic with tools like Next.js, truly stood out. If you're seeking to build a reliable and high-performing backend system, Yomna is the ideal partner.",
+    name: "Dr. Alaa Zaghloul",
+    title: "Head of Computer Science Department, Misr University for Science and Technology",
+  },
+  {
+    quote:
+      "Yomna is a highly talented engineer with strong expertise across both full-stack development and data science. She consistently demonstrates a sharp ability to tackle complex technical challenges and she delivers elegant and effective solutions. Yomna is an excellent collaborator as she demonstrated through group discussions and proactively driving team progress. I give her my highest recommendation.",
+    name: "Flavia Trotolo",
+    title: "Investment Software Engineer at Liwa Capital Advisors",
+  },
+  {
+    quote:
+      "Working alongside Yomna was an inspiring experience. Their professionalism, quick problem-solving, and enthusiasm for full stack backend development were contagious throughout our internship. Yomna's knack for optimizing server-side logic and integrating reliable solutions with Next.js and Vercel deployment processes was remarkable. If you're looking for a collaborative and skilled teammate to elevate your backend systems, Yomna is an outstanding choice.",
+    name: "Basma Sabour",
+    title: "Fellow Intern at Misr University for Science and Technology",
+  },
+  {
+    quote:
+      "It was a privilege to mentor Yomna during during her internship. Her professionalism, proactive approach, and dedication to crafting robust backend systems were truly impressive. Yomna's ability to design efficient APIs and ensure seamless server-side performance using Express JS and tools like Sentry demonstrated exceptional skill. Her innovative solutions and commitment to excellence make Yomna an invaluable asset for any team aiming to build scalable and secure backend infrastructure.",
+    name: "Kareem Mohammed",
+    title: "Cyber Security Engineer at Smart Village, Internship Mentor",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Cyber Security Intern",
+    desc: "Engaged in penetration testing and capture-the-flag challenges, enhancing cybersecurity skills as a cybersecurity intern.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Mobile App Dev",
+    desc: "Designed and developed the backend of a mobile app for both iOS & Android platforms using Express JS.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Freelance App Dev Project",
+    desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Lead Backend Developer",
+    desc: "Developed and maintained an AI-powered Lupus Diagnosis Assistant mobile app features using modern backend technologies.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+  },
+];
